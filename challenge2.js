c22d8ba9b5ab9287e742b0972905c8081ba1fd3f
macdonald34 <144123866+macdonald34@users.noreply.github.com>
2023-10-02 15:22:01 +0300

    Completed code challenge 1

--- challenge2.js
+++ challenge2.js
@@ -1,35 +1,45 @@
+// Solution
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process. stdin,
-    output: process. stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question('Enter the vehicle\'s speed: ',(userInput) => {
-    const speed = parseFloat(userInput);
-    if( 1sNaN (speed));{
-        speedDetactor(speed);
-    } else {
-        console.log("Invalid input. please enter a valid speed.");
-    }
 
-    rl.close();
+rl.question('Enter the vehicle\'s speed: ', (userInput) => {
+  const speed = parseFloat(userInput);
+  
+  if (!isNaN(speed)) {
+    speedDetector(speed);
+  } else {
+    console.log("Invalid input. Please enter a valid speed.");
+  }
+
+  rl.close();
 });
 
-function speedDetactor(speed)
+
+
+
+
+
+function speedDetector(speed)
 {
     let demerit = 0
     if(speed<70){
         console.log("OK");
-         
-            let difference = speed-70
-            demerit = difference / 5
+    }
+    else{
+        let difference = speed-70
+        demerit = difference / 5
 
-            if (demerit > 12){
-                console.log("License suspended")
-            }
-            else{
-                console.log('points ${demerit}')
-            }
+        if (demerit > 12){
+            console.log("License Suspended")
+        }
+        else{
+            console.log(`Points ${demerit}`)
         }
     }
+}
